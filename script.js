ab35735af2654758984c4b5a4c297318414889be
Athira RK <athirarr764@gmail.com>
2026-04-01 14:33:19 +0530

    Add weather icons to the weather icon container

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ searchInput.addEventListener("keydown", (event) => {
 
 // Fetching Weather Data 
 async function getWeather() {
-    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Kochi&appid=6a524c9afeac3b039318a2ede31c3cc6&units=metric";
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=France&appid=6a524c9afeac3b039318a2ede31c3cc6&units=metric";
     try {
         const response = await fetch(apiURL);
         if(!response.ok) {
@@ -84,6 +84,31 @@ async function getWeather() {
         const highTemp = Math.round(data.main.temp_max);
         const lowTemp = Math.round(data.main.temp_min);
 
+        const iconMap = {
+            "01d": "assets/01d.png",
+            "01n": "assets/01n.png",
+            "02d": "assets/02d.png",
+            "02n": "assets/02n.png",
+            "03d": "assets/03d.png",
+            "03n": "assets/03n.png",
+            "04d": "assets/04d.png",
+            "04n": "assets/04n.png",
+            "09d": "assets/09d.png",
+            "09n": "assets/09n.png",
+            "10d": "assets/10d.png",
+            "10n": "assets/10n.png",
+            "11d": "assets/11d.png",
+            "11n": "assets/11n.png",
+            "13d": "assets/13d.png",
+            "13n": "assets/13n.png",
+            "50d": "assets/50d.png",
+            "50n": "assets/50n.png"
+        };
+        const iconSrc = iconMap[weatherIconCode] || "assets/cloudy.png";
+
+
+
+        document.querySelector(".weatherImg").src = iconSrc;
         document.querySelector(".weather-name").textContent = weatherDescription.charAt(0).toUpperCase() + 
         weatherDescription.slice(1);
         document.querySelector(".feels-like").textContent = `Feels like ${feelsLike}°C`;
